5039aa430cc26ddb93ccdbe7dca34984a5da2b3e
Vivek4224 <vivekbarvaliya4224@gmail.com>
2024-09-15 20:50:37 +0530

    add

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Navbar from "./Sections/Navbar";
+import Hero from "./Sections/Hero";
 
 const App = () => {
   return (
     <main className="max-w-7xl mx-auto">
       <Navbar />
+      <Hero />
     </main>
   );
 };
